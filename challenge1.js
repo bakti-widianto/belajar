ea52e9c23d8802b6706298c3b83a133579664b8e
Bhakti <bakti.widianto@gmail.com>
2020-04-15 11:37:03 +0700

    first commit

---
+++ challenge1.js
@@ -0,0 +1,37 @@
+//membuat function penjumlahan
+
+// function sum(){
+//     var x = 0
+//     for(var i = 0; i < arguments.length; i++){
+//         x += arguments[i];
+
+//     }
+//  console.log(x);
+// }
+
+
+// sum(1,2,7)
+// sum(1,4)
+// sum(11)
+// sum(10,3,6,7,9)
+
+function sum(){ 
+let bil = 0;
+for (let i = 0; i < arguments.length; i++) {
+    bil += arguments[i]
+
+
+}
+console.log(bil)
+}
+
+sum(2,3)
+
+
+
+
+
+
+
+
+
